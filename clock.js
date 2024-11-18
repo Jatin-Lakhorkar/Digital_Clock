bd0efe07c9d7be39350e9ab0feea2018b35a684d
Jatin-Lakhorkar <jatin.lakhorkar@gmail.com>
2024-11-18 15:21:31 +0530

    test

--- clock.js
+++ clock.js
@@ -3,7 +3,7 @@ function updateClock() {
     let hours = now.getHours();
     const meridian = hours >= 12 ? "PM" : "AM";
     hours = hours % 12 || 12;
-    hours = hours.toString().padStart(2, 0);
+    hours = hours.toString().padStart(2, 0);//hour
     const minutes = now.getMinutes().toString().padStart(2, 0);
     const seconds = now.getSeconds().toString().padStart(2, 0);
     const timeString = `${hours}:${minutes}:${seconds} ${meridian}`;
